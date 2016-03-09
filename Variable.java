5e7fcce577fbe1f8c1eba0f52730c10f8ad69ce7
COD3PROGRAM <hidayatullah.4fif@gmail.com>
2016-03-09 15:50:06 +0700

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorial_java;
+
+/**
+ *
+ * @author code
+ */
+public class Variable {
+    public static void main(String[] args){
+        int angka = 15;
+        char y = 'A';
+        String kalimat = "Succes Code";
+        boolean benar = true;
+        double koma = 1.5;
+        
+        System.out.println("nilai anda adalah " +angka);
+        System.out.println("nama depan anda adalah"  +y);
+        System.out.println("nilai koma anda yg anda dapatkan adalah" +koma);
+        System.out.println(kalimat);
+        System.out.println(benar);
+                }
+    
+}
